d74a946aadeacd18da1334e62a578b36b6a68fa7
sysnar <sysnar@gmail.com>
2021-07-12 23:39:51 +0900

    React component usages #002

--- src/App.js
+++ src/App.js
@@ -1,41 +1,8 @@
-import './App.css';
 import React, { Component } from 'react';
-
-class Subject extends Component {
-  render() {
-    return (
-      <header>
-        <h1>{this.props.title}</h1>
-        {this.props.sub}
-      </header>
-    )
-  }
-}
-
-class TOC extends Component {
-  render() {
-    return (
-      <nav>
-        <ul>
-          <li><a href="1.html">HTML</a></li>
-          <li><a href="2.html">CSS</a></li>
-          <li><a href="3.html">JAVASCRIPT</a></li>
-        </ul>
-      </nav>
-    )
-  }
-}
-
-class Content extends Component {
-  render() {
-    return (
-      <article>
-        <h2>HTML</h2>
-        HTML is HyperText Markup Language.
-      </article>
-    )
-  }
-}
+import TOC from './components/TOC';
+import Subject from './components/Subject';
+import Content from './components/Content';
+import './App.css';
 
 class App extends Component {
   render() {
