013b8492b6f6d4bd4888056bbe1e7bc3e386c5fd
sysnar <sysnar@gmail.com>
2021-07-13 00:07:14 +0900

    Fundamental state usages #003

--- src/App.js
+++ src/App.js
@@ -5,11 +5,24 @@ import Content from './components/Content';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: {title:'WEB', sub:'World Wide Web'},
+      content:[
+        {id:1, title:'HTML', desc:'HTML is for informaiton'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
-        <TOC></TOC>
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}></Subject>
+        <TOC data={this.state.content}></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     )
