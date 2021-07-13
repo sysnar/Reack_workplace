20257e3525d3e65cd2fbdc4aaaf0189f68904c99
sysnar <sysn4r@gmail.com>
2021-07-14 00:59:04 +0900

    State value change with onClick/bind/setState Func #004

--- src/components/Subject.js
+++ src/components/Subject.js
@@ -2,9 +2,14 @@ import React, {Component} from 'react';
 
 class Subject extends Component {
   render() {
+    console.log('Subject render');
     return (
       <header>
-        <h1>{this.props.title}</h1>
+        <h1><a href="/" onClick={function(e) {
+          console.log(e);
+          e.preventDefault();
+          // alert('hello react');
+        }}>{this.props.title}</a></h1>
         {this.props.sub}
       </header>
     )
